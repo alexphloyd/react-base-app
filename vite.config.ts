f6c63d7700f1672d3010c6481fac4a2df56d212a
alexphloyd <alexphloyd@gmail.com>
2023-07-02 11:53:05 -0500

    setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import react from '@vitejs/plugin-react';
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {},
 });
